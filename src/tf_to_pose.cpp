921619a6a67082df6386138a2a89c21fd46a7b64
Łukasz Mitka <lukasz.mitka@husarion.com>
2018-11-06 11:13:20 +0100

    Added frame and topic parameters

--- src/tf_to_pose.cpp
+++ src/tf_to_pose.cpp
@@ -8,24 +8,31 @@ geometry_msgs::PoseStamped rosbot_pose;
 
 int main(int argc, char **argv)
 {
-
   ros::init(argc, argv, "tf_to_pose");
   tf::TransformListener listener;
 
-  ros::NodeHandle node;
-  rosbot_map_pub = node.advertise<geometry_msgs::PoseStamped>("/rosbot_on_map_pose", 1);
+  ros::NodeHandle node("~");
+  std::string parent_frame;
+  std::string child_frame;
+  std::string out_topic;
+  node.param<std::string>("parent_frame", parent_frame, "/map");
+  node.param<std::string>("child_frame", child_frame, "/base_link");
+  node.param<std::string>("out_topic", out_topic, "/rosbot_on_map_pose");
 
+  rosbot_map_pub = node.advertise<geometry_msgs::PoseStamped>(out_topic, 1);
+
+  rosbot_pose.header.frame_id = parent_frame;
   ros::Rate rate(10.0);
   while (node.ok())
   {
     tf::StampedTransform transform;
     try
     {
-      listener.lookupTransform("/map", "/base_link", ros::Time(0), transform);
+      listener.lookupTransform(parent_frame, child_frame, ros::Time(0), transform);
     }
     catch (tf::TransformException ex)
     {
-      ROS_WARN("Could not get transform between 'map' and 'base_link', will retry every second");
+      ROS_WARN("Could not get transform between '%s' and '%s', will retry every second", parent_frame.c_str(), child_frame.c_str());
       ros::Duration(1.0).sleep();
     }
 
